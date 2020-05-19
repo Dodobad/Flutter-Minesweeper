732d1ad09a20536d6a690e73435e8d38626350f1
Dodobad <56236646+Dodobad@users.noreply.github.com>
2020-05-18 21:30:22 -0400

    Generated boilerplate
    Project created and added to Git

---
+++ minesweeper/android/app/src/main/java/tpradan/minesweeper/MainActivity.java
@@ -0,0 +1,6 @@
+package tpradan.minesweeper;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ minesweeper/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+
+enum TileState {covered, wrong, open, flagged, reveal}
+
+
+void main() => runApp(MineSweeper());
+
+class MineSweeper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MineSweeper",
+      home: Board(),
+    );
+  }
+}
+
+class Board extends StatefulWidget {
+  @override
+  _BoardState createState() => _BoardState();
+}
+
+class _BoardState extends State<Board> {
+  final int rows = 9;
+  final int cols = 9;
+  final int mines = 11;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
