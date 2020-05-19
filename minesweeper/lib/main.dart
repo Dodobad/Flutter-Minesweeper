bd966748b292112b9c14dc46b3c7090fe35f6a57
Dodobad <56236646+Dodobad@users.noreply.github.com>
2020-05-18 21:34:59 -0400

    Loose generation file removed

--- minesweeper/lib/main.dart
+++ minesweeper/lib/main.dart
@@ -25,7 +25,15 @@ class _BoardState extends State<Board> {
   final int rows = 9;
   final int cols = 9;
   final int mines = 11;
-  
+  List<List<TileState>> stateUI;
+
+
+  void reset() {
+    stateUI = new List<List<TileState>>.generate(rows, (row) {
+      return new List<TileState>.filled(cols, TileState.covered);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
