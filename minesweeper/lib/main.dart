c7540b91e8d23b4c4b2037c468f170b5512dd431
Dodobad <56236646+Dodobad@users.noreply.github.com>
2020-06-03 13:17:44 -0400

    Full functionality

--- minesweeper/lib/main.dart
+++ minesweeper/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
@@ -28,7 +30,29 @@ class _BoardState extends State<Board> {
   List<List<TileState>> stateUI;
   List<List<bool>> tile;
 
+  bool alive;
+  bool won;
+  int minesFound;
+  Timer timer;
+  Stopwatch watch = Stopwatch();
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
   void reset() {
+    alive = true;
+    won = false;
+    minesFound = 0;
+    watch.reset();
+    timer?.cancel();
+
+    timer = Timer.periodic(Duration(seconds: 1), (Timer timer) {
+      setState(() {});
+    });
+
     stateUI = new List<List<TileState>>.generate(rows, (row) {
       return new List<TileState>.filled(cols, TileState.covered);
     });
@@ -57,19 +81,26 @@ class _BoardState extends State<Board> {
   }
 
   Widget newBoard() {
+    bool hasCoveredCell = false;
     List<Row> boardRow = <Row>[];
     for (int y = 0; y < rows; y++) {
       List<Widget> rowChild = <Widget>[];
       for (int x = 0; x < cols; x++) {
         TileState state = stateUI[y][x];
-        int count = mineCount(x,y);
+        int count = mineCount(x, y);
+        if (alive != null) {
+          if (!alive) {
+            if (state != TileState.wrong)
+              state = tile[y][x] ? TileState.reveal : state;
+          }
+        }
         if (state == TileState.covered || state == TileState.flagged) {
           rowChild.add(GestureDetector(
             onLongPress: () {
-              flag(x,y);
+              flag(x, y);
             },
             onTap: () {
-              probe(x,y);
+              if (state == TileState.covered) probe(x, y);
             },
             child: Listener(
               child: CoveredTile(
@@ -79,6 +110,9 @@ class _BoardState extends State<Board> {
               ),
             ),
           ));
+          if (state == TileState.covered) {
+            hasCoveredCell = true;
+          }
         } else {
           rowChild.add(OpenTile(state, count));
         }
@@ -88,6 +122,12 @@ class _BoardState extends State<Board> {
         mainAxisAlignment: MainAxisAlignment.center,
         key: ValueKey<int>(y),
       ));
+    }
+    if (!hasCoveredCell) {
+      if ((minesFound == mines) && alive) {
+        won = true;
+        watch.stop();
+      }
     }
     return Container(
       color: Colors.grey[700],
@@ -100,9 +140,46 @@ class _BoardState extends State<Board> {
 
   @override
   Widget build(BuildContext context) {
+    int timeElapsed = watch.elapsedMilliseconds ~/ 1000;
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
         title: Text("MineSweeper"),
+        centerTitle: true,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(45.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FlatButton(
+                child: Text(
+                  'Reset Board',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () => reset(),
+                highlightColor: Colors.red,
+                shape: StadiumBorder(
+                  side: BorderSide(
+                    color: Colors.blue[200],
+                  ),
+                ),
+                color: Colors.blueAccent[100],
+              ),
+              Container(
+                height: 40.0,
+                alignment: Alignment.center,
+                child: RichText(
+                  text: TextSpan(
+                      text: won
+                          ? "Congratulations!  $timeElapsed seconds of play."
+                          : alive
+                              ? "Found $minesFound of $mines mines. Current playtime: $timeElapsed"
+                              : "Lost the game! $timeElapsed seconds of play."),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
       body: Container(
         color: Colors.grey[50],
@@ -113,60 +190,66 @@ class _BoardState extends State<Board> {
     );
   }
 
-  void probe (int x, int y) {
+  void probe(int x, int y) {
+    if (!alive) return;
     if (stateUI[y][x] == TileState.flagged) return;
     setState(() {
-      if(tile[y][x]) {
+      if (tile[y][x]) {
         stateUI[y][x] = TileState.wrong;
+        alive = false;
+        timer.cancel();
       } else {
-        open(x,y);
+        open(x, y);
+        if (!watch.isRunning) watch.start();
       }
     });
   }
 
-  void open(int x , int y) {
-    if (!inBoard(x,y)) return;
+  void open(int x, int y) {
+    if (!inBoard(x, y)) return;
     if (stateUI[y][x] == TileState.open) return;
     stateUI[y][x] = TileState.open;
-    if(mineCount(x, y) > 0) return;
+    if (mineCount(x, y) > 0) return;
 
-    open(x-1, y);
-    open(x+1, y);
-    open(x-1, y-1);
-    open(x-1, y+1);
-    open(x+1, y-1);
-    open(x-1, y+1);
-    open(x, y-1);
-    open(x, y+1);
+    open(x - 1, y);
+    open(x + 1, y);
+    open(x - 1, y - 1);
+    open(x - 1, y + 1);
+    open(x + 1, y - 1);
+    open(x - 1, y + 1);
+    open(x, y - 1);
+    open(x, y + 1);
   }
 
   void flag(int x, int y) {
+    if (!alive) return;
     setState(() {
-      if(stateUI[y][x] == TileState.flagged){
+      if (stateUI[y][x] == TileState.flagged) {
         stateUI[y][x] = TileState.covered;
+        --minesFound;
       } else {
         stateUI[y][x] = TileState.flagged;
+        ++minesFound;
       }
     });
   }
 
   int mineCount(int x, int y) {
     int count = 0;
-    count+= bombs(x-1, y);
-    count+= bombs(x+1, y);
-    count+= bombs(x, y-1);
-    count+= bombs(x, y+1);
-    count+= bombs(x-1, y-1);
-    count+= bombs(x-1, y+1);
-    count+= bombs(x+1, y-1);
-    count+= bombs(x+1, y+1);
+    count += bombs(x - 1, y);
+    count += bombs(x + 1, y);
+    count += bombs(x, y - 1);
+    count += bombs(x, y + 1);
+    count += bombs(x - 1, y - 1);
+    count += bombs(x - 1, y + 1);
+    count += bombs(x + 1, y - 1);
+    count += bombs(x + 1, y + 1);
     return count;
   }
 
   int bombs(int x, int y) => inBoard(x, y) && tile[y][x] ? 1 : 0;
 
-  bool inBoard(int x, int y) => x>= 0 && x < cols && y >= 0 && y < rows;
-
+  bool inBoard(int x, int y) => x >= 0 && x < cols && y >= 0 && y < rows;
 }
 
 Widget buildTile(Widget child) {
